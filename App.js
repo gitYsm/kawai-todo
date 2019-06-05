fb6ce6250eaff1e28154358f80206ba805be64bb
gitYsm <yangmo112p@gmail.com>
2019-06-05 17:35:58 +0900

    lectur4 Finished

--- App.js
+++ App.js
@@ -2,22 +2,42 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput,
    Dimensions, Platform
    } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 //import { TextInput } from 'react-native-gesture-handler';
-
+import ToDo from './ToDo'
 const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            value={newToDo} 
+            onChangeText={this._crontolNewToDo} 
+            returnKeyType={"done"}
+            autoCorrect={false}  
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
+  _crontolNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -52,5 +72,14 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth: 1,
+    fontSize: 20
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
